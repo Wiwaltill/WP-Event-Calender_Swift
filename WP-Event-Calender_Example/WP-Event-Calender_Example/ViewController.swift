c2f3824bfc0cdef760c156a1a8f50b450e7b0493
TiLL Esser <74987472+Wiwaltill@users.noreply.github.com>
2022-10-16 22:36:28 +0200

    Create Project

---
+++ WP-Event-Calender_Example/WP-Event-Calender_Example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WP-Event-Calender_Example
+//
+//  Created by Till Esser on 16.10.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
