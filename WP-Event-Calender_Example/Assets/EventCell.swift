e74f8bf0bfe8e8ab4f0815eeffb173c9a0b6a69b
TiLL Esser <74987472+Wiwaltill@users.noreply.github.com>
2022-10-16 22:57:55 +0200

    Create EventCell.swift

---
+++ WP-Event-Calender_Example/Assets/EventCell.swift
@@ -0,0 +1,16 @@
+//
+//  EventCell.swift
+//  WP-Event-Calender_Example
+//
+//  Created by Wiwaltill on 16.10.22.
+//
+
+import UIKit
+
+class EventCell: UITableViewCell{
+    
+    @IBOutlet var title : UILabel?
+    @IBOutlet var detail : UILabel?
+    @IBOutlet var date : UILabel?
+}
+
